e0f3cf1f407e208d34f78b25c488f46cdbd546ce
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-15 01:42:55 +0500

    git folder taken to parent folder

---
+++ test_drive/lib/main.dart
@@ -0,0 +1,35 @@
+import "package:english_words/english_words.dart";
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Welcome to flutter"),
+        ),
+        body: Center(
+          child: RandomWords(),
+        ),
+      ),
+    );
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
