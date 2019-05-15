3b0436cd64d99d09968ffb5daac62de2bdf72d3d
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-15 20:12:27 +0500

    List View added

--- test_drive/lib/main.dart
+++ test_drive/lib/main.dart
@@ -9,15 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Welcome to flutter"),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 }
