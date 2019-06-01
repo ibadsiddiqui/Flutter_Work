28319066f98805658ecd05847fd05a14fad72c79
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-06-01 23:45:47 +0500

    --Commit: dart variable name added

---
+++ Working_With_Dart/04-VariableNaming.dart
@@ -0,0 +1,10 @@
+main(List<String> args) {
+  var name1 = 'Bob';
+  int lineCount;
+  assert(lineCount == null);
+
+  // using Final keyword or const non-changeable values
+  final name2 = 'Bob'; // Without a type annotation
+  final String nickname3 = 'Bobby';
+
+}
