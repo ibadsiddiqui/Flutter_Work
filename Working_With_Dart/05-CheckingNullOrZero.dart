75122e0fc351e2b1bb1f5ef0ee6434df6e9fa38c
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-06-01 23:47:19 +0500

    --Commit: Checking for null or zero

---
+++ Working_With_Dart/05-CheckingNullOrZero.dart
@@ -0,0 +1,13 @@
+main(List<String> args) {
+  // Dart
+  var myNull = null;
+  if (myNull == null) {
+    print('use "== null" to check null');
+  }
+  var zero = 0;
+  if (zero == 0) {
+    print('use "== 0" to check zero');
+  }
+  
+  print(0 == null);
+}
