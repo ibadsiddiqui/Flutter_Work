bceb97a957ba9640541455bd4e36e8615ce9a424
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-06-03 01:00:48 +0500

    --Commit: statefull widget added

--- Flutter-Full-Work/01-Section/basic_project/lib/main.dart
+++ Flutter-Full-Work/01-Section/basic_project/lib/main.dart
@@ -4,26 +4,45 @@ void main(List<String> args) {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Text("Salam"),
+//             Center(
+//               child: FlatButton(
+//                 child: Text("Press Me",textScaleFactor: 1.2,),
+//                 onPressed: (){
+//                   print("Salam");
+//                 },
+//               )
+//             ) 
+//           ]
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Salam"),
-            Center(
-              child: FlatButton(
-                child: Text("Press Me",textScaleFactor: 1.2,),
-                onPressed: (){
-                  print("Salam");
-                },
-              )
-            ) 
-          ]
-        ),
-      ),
+       home: Scaffold(
+         body: Center(
+           child: Text("Ibad here"),
+         ),
+       ) 
     );
   }
 }
