ba79979d0c530619ded9b9f0554070b87cb2b7b7
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-06-03 01:51:01 +0500

    --Commit: working with plugin

--- Flutter-Full-Work/01-Section/basic_project/lib/main.dart
+++ Flutter-Full-Work/01-Section/basic_project/lib/main.dart
@@ -1,29 +1,27 @@
 import 'package:flutter/material.dart';
-
-void main(List<String> args) {
-  runApp(MyApp());
-}
+import "package:system_shortcuts/system_shortcuts.dart";
 
 class MyApp extends StatefulWidget {
   MyApp({Key key}) : super(key: key);
-
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  String value = "Ibad siddiqui here";
-
-  @override
-  void initState() {
-    super.initState();
-    print("state intiailized");
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      body: Center(child: Image.asset("assets/images/hug.png")),
-    ));
+            body: Center(
+      child: FlatButton(
+        child: Text("Home"),
+        onPressed: () async {
+          await SystemShortcuts.home();
+        },
+      ),
+    )));
   }
+}
+
+void main(List<String> args) {
+  runApp(MyApp());
 }
