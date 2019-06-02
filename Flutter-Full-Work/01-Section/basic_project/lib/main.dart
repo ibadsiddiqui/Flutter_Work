b403879e6bcc00303f880191e00f3336d49fe0ca
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-06-03 01:24:26 +0500

    --Commit: Image asset rendered in the main

--- Flutter-Full-Work/01-Section/basic_project/lib/main.dart
+++ Flutter-Full-Work/01-Section/basic_project/lib/main.dart
@@ -4,30 +4,6 @@ void main(List<String> args) {
   runApp(MyApp());
 }
 
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         body: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text("Salam"),
-//             Center(
-//               child: FlatButton(
-//                 child: Text("Press Me",textScaleFactor: 1.2,),
-//                 onPressed: (){
-//                   print("Salam");
-//                 },
-//               )
-//             )
-//           ]
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 class MyApp extends StatefulWidget {
   MyApp({Key key}) : super(key: key);
 
@@ -38,31 +14,16 @@ class _MyAppState extends State<MyApp> {
   String value = "Ibad siddiqui here";
 
   @override
-  void initState() { 
+  void initState() {
     super.initState();
-    print("state intiailized");  
+    print("state intiailized");
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Center(
-              child: Text(value),
-            ),
-            FlatButton(child: Text("Press Me To Change"), 
-              onPressed: () {
-                value="Ibad here";
-                print(value);
-                setState(() {});
-              },
-            )
-          ],
-        )
-      )
-    );
+        home: Scaffold(
+      body: Center(child: Image.asset("assets/images/hug.png")),
+    ));
   }
 }
