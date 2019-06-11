8e7d8a755bc723230cc442b2770fd11867d9f788
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-06-11 18:18:29 +0500

    --Commit: working with rows, columns and containers

--- Flutter-Full-Work/02-The building blocks and widgets of Flutter/building_block_and_widgets/lib/main.dart
+++ Flutter-Full-Work/02-The building blocks and widgets of Flutter/building_block_and_widgets/lib/main.dart
@@ -15,29 +15,34 @@ class MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
+        body: Container(
+            alignment: AlignmentDirectional(0.0, 0.0),
             child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Center(child: Image.asset("assets/images/hug.png", width: 100.0,height: 100.0,)),
-            // Center(child: Image.network("https://previews.123rf.com/images/merkulovnik/merkulovnik1802/merkulovnik180200138/96702524-ink-drop-in-water-background.jpg", width: 100.0,height: 100.0,)),
-            
-            Chip(
-                label: Text(str,
-                    style: TextStyle(
-                        color: Colors.teal, fontWeight: FontWeight.bold))),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: OutlineButton(
-                onPressed: () {
-                  str = "The text just changed I think";
-                  setState(() {});
-                },
-                child: Text("Press Me"),
-              ),
-            )
-          ],
-        )),
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text('Ibad Here'),
+                    FlatButton(
+                      child: Text('Press me'),
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text('Ibad Here'),
+                    FlatButton(
+                      child: Text('Press me'),
+                      onPressed: () {},
+                    )
+                  ],
+                )
+              ],
+            )),
       ),
     );
   }
