a76eb935d0fbaea4508cf2cfc9e2084c046f6b27
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-06-17 20:51:36 +0500

    --Commit: Container decoration along with box decoration styles added

--- Flutter-Full-Work/02-The building blocks and widgets of Flutter/building_block_and_widgets/lib/main.dart
+++ Flutter-Full-Work/02-The building blocks and widgets of Flutter/building_block_and_widgets/lib/main.dart
@@ -5,15 +5,16 @@ void main() => runApp(App());
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MainPage(); 
+    return MainPage();
   }
 }
 
-class MainPage extends  StatefulWidget {
+class MainPage extends StatefulWidget {
   MainPage({Key key}) : super(key: key);
 
   _MainPageState createState() => _MainPageState();
 }
+
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
@@ -21,12 +22,11 @@ class _MainPageState extends State<MainPage> {
   }
 }
 
-
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
-
 }
+
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
@@ -35,10 +35,16 @@ class _HomeState extends State<Home> {
 
     return Scaffold(
         body: Container(
-      alignment: Alignment.center,
-      color: Colors.teal,
-      child: Text('ibad'),
-      constraints: BoxConstraints(maxHeight: height, maxWidth: width),
-    ));
+            alignment: Alignment.center,
+            // color: Colors.teal,
+            child: Text('ibad'),
+            // constraints: BoxConstraints(maxHeight: height, maxWidth: width),
+            padding: EdgeInsets.all(10.0),
+            margin: EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+                // color: Colors.teal
+                // gradient: LinearGradient(colors: [Colors.teal, Colors.tealAccent])),
+                // gradient: RadialGradient(colors: [Colors.teal, Colors.tealAccent])),
+                )));
   }
 }
