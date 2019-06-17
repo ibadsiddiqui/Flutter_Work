04ed065ec79833b488be1f7ca04083ca068e77b1
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-06-17 20:45:35 +0500

    --Commit: Media Query added for width and height

--- Flutter-Full-Work/02-The building blocks and widgets of Flutter/building_block_and_widgets/lib/main.dart
+++ Flutter-Full-Work/02-The building blocks and widgets of Flutter/building_block_and_widgets/lib/main.dart
@@ -1,26 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Main());
+void main() => runApp(App());
 
-class Main extends StatefulWidget {
+class App extends StatelessWidget {
   @override
-  _MainState createState() {
-    return new _MainState();
+  Widget build(BuildContext context) {
+    return MainPage(); 
   }
 }
 
-class _MainState extends State<Main> {
-  String str = "Salam, My Name is Ibad";
+class MainPage extends  StatefulWidget {
+  MainPage({Key key}) : super(key: key);
+
+  _MainPageState createState() => _MainPageState();
+}
+class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return MaterialApp(home: Home());
+  }
+}
+
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+
+}
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
         body: Container(
-          alignment: Alignment.center,
-          color: Colors.teal,
-          child: Text('ibad here'),
-        ),
-      ),
-    );
+      alignment: Alignment.center,
+      color: Colors.teal,
+      child: Text('ibad'),
+      constraints: BoxConstraints(maxHeight: height, maxWidth: width),
+    ));
   }
 }
