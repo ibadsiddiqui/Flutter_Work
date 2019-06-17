3837ae7c5fc4d6ea4e9532b47e476d9947792149
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-06-17 20:08:11 +0500

    --Commit: Container background color and content alignment added

--- Flutter-Full-Work/02-The building blocks and widgets of Flutter/building_block_and_widgets/lib/main.dart
+++ Flutter-Full-Work/02-The building blocks and widgets of Flutter/building_block_and_widgets/lib/main.dart
@@ -1,41 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Main());
 
-class MyApp extends StatefulWidget {
+class Main extends StatefulWidget {
   @override
-  MyAppState createState() {
-    return new MyAppState();
+  _MainState createState() {
+    return new _MainState();
   }
 }
 
-class MyAppState extends State<MyApp> {
+class _MainState extends State<Main> {
   String str = "Salam, My Name is Ibad";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          body: ListView.separated(
-              itemBuilder: (context, int index) {
-                return ListTile(
-                  leading: Icon(Icons.account_box),
-                  title: Text("Salam" + index.toString()),
-                  onTap: () {
-                    print(index);
-                  },
-                );
-              },
-              itemCount: 20,
-              separatorBuilder: (context, index) {
-                if ((index + 1) % 5 == 0) {
-                  return ListTile(
-                    title: Text((index + 1).toString()),
-                    selected: true,
-                  );
-                } else {
-                  return Container();
-                }
-              })),
+        body: Container(
+          alignment: Alignment.center,
+          color: Colors.teal,
+          child: Text('ibad here'),
+        ),
+      ),
     );
   }
 }
