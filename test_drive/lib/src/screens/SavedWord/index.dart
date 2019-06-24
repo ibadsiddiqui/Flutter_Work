b530c159f6c4428a41531480a6b7a7d102845fbc
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-06-24 21:55:58 +0500

    --Commit Iterable list added for saved words

---
+++ test_drive/lib/src/screens/SavedWord/index.dart
@@ -0,0 +1,33 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class SavedWordsScreen extends StatelessWidget {
+  Set<WordPair> saved = Set<WordPair>();
+
+  SavedWordsScreen({Key key, @required this.saved}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Iterable<ListTile> tiles = saved.map(
+      (WordPair pair) {
+        return ListTile(
+          title: Text(
+            pair.asPascalCase,
+            style: TextStyle(fontSize: 18.0),
+          ),
+        );
+      },
+    );
+    final List<Widget> divided = ListTile.divideTiles(
+      context: context,
+      tiles: tiles,
+    ).toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Saved Suggestions'),
+      ),
+      body: ListView(children: divided),
+    );
+  }
+}
