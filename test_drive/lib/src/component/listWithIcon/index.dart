74078256f67baeec1edd76c9c33af0cb1ceeb984
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-06-24 21:54:00 +0500

    --Commit: List generator added

--- test_drive/lib/src/component/listWithIcon/index.dart
+++ test_drive/lib/src/component/listWithIcon/index.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:english_words/english_words.dart';
 
-Widget rowBuilder(pair, alreadySaved, updateList) {
+Widget ListOfWords(_suggestions, _saved, updateList) {
+  return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return Divider(); /*2*/
+
+        final index = i ~/ 2; /*3*/
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        }
+        return rowBuilder(_suggestions[index], _saved, updateList);
+      });
+}
+
+Widget rowBuilder(WordPair pair, _saved, updateList) {
+  final bool alreadySaved = _saved.contains(pair);
   final TextStyle _biggerFont = TextStyle(fontSize: 18.0);
 
   return ListTile(
