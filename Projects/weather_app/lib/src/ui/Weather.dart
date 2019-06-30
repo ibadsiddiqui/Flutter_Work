671cfde5091a75df53c5cc711d1f78b524fc032a
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-06-30 20:12:06 +0500

    --Commit: code separated for date time

--- Projects/weather_app/lib/src/ui/Weather.dart
+++ Projects/weather_app/lib/src/ui/Weather.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/src/component/common/DateTime.dart';
+
+// Widget dateTime() {
+//   return Container(
+//       child: Text(
+//     (new DateFormat.yMMMd().format(new DateTime.now())),
+//     style: TextStyle(
+//         fontWeight: FontWeight.bold, color: Colors.white, fontSize: 24.0),
+//   ));
+// }
 
 class Weather extends StatelessWidget {
   @override
@@ -7,7 +17,7 @@ class Weather extends StatelessWidget {
       padding: EdgeInsets.all(60.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[],
+        children: <Widget>[dateTime(24.0, FontWeight.bold, Colors.white)],
       ),
     );
   }
