5d313b54b7a7ab9dfec21e0d86fe1687afae81f7
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-06-30 19:55:47 +0500

    --Commit: Weather added for home page

---
+++ Projects/weather_app/lib/src/ui/Weather.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Weather extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(60.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[],
+      ),
+    );
+  }
+}
