1d225d87c681ee107d471b818973997f0078f30e
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-07-01 01:04:41 +0500

    --Commit: blackground made white

--- Projects/weather_app/lib/src/ui/homepage.dart
+++ Projects/weather_app/lib/src/ui/homepage.dart
@@ -18,7 +18,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlue,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(widget.title),
       ),
