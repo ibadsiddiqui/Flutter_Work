67d355d14e718d74a4cb02b9b1d666b31f1b0711
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-07-01 21:22:33 +0500

    --Commit: city description added

--- Projects/weather_app/lib/src/ui/Weather.dart
+++ Projects/weather_app/lib/src/ui/Weather.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/src/component/common/CityAndWeather.dart';
 import 'package:weather_app/src/component/common/DateTime.dart';
 import 'package:weather_app/src/component/common/Temperature.dart';
 
 class Weather extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-  
+    
     return Container(
       padding: EdgeInsets.all(60.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           dateTime(24.0, FontWeight.bold, Colors.black),
-          TempSection(80.0, "assets/img/cloudy.png")
+          TempSection(80.0, "assets/img/cloudy.png"),
+          city('Tokyo', 'Cloudy')
         ],
       ),
     );
