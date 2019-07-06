21390fb800fc3ed5c6e8cbf9158f1de5ec6e8d05
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-07-06 13:52:18 +0500

    --Commit: weather model added and mapped

--- Projects/weather_app/lib/src/ui/Weather.dart
+++ Projects/weather_app/lib/src/ui/Weather.dart
@@ -2,19 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:weather_app/src/component/common/CityAndWeather.dart';
 import 'package:weather_app/src/component/common/DateTime.dart';
 import 'package:weather_app/src/component/common/Temperature.dart';
+import 'package:weather_app/src/model/Weather.model.dart';
 
 class Weather extends StatelessWidget {
+  final WeatherModel weatherModel;
+
+  Weather({@required this.weatherModel});
+
   @override
   Widget build(BuildContext context) {
-    
     return Container(
       padding: EdgeInsets.all(60.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           dateTime(24.0, FontWeight.bold, Colors.black),
-          TempSection(80.0, "assets/img/cloudy.png"),
-          city('Tokyo', 'Cloudy')
+          TempSection(weatherModel.temp.toStringAsFixed(0), 80.0,
+              "https://openweathermap.org/img/w/${weatherModel.icon}.png"),
+          city(weatherModel.name, weatherModel.main)
         ],
       ),
     );
