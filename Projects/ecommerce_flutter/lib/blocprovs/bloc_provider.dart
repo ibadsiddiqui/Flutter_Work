4e3ade6dc9352362ec66546e4724a2960813179c
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-17 00:25:59 +0500

    --Commit: BLoC provder added

--- Projects/ecommerce_flutter/lib/blocprovs/bloc_provider.dart
+++ Projects/ecommerce_flutter/lib/blocprovs/bloc_provider.dart
@@ -1,6 +1,48 @@
 // Generic Interface for all BLoCs
+import 'package:flutter/widgets.dart';
+
 abstract class BlocBase {
   void dispose();
 }
 
 // Generic BLoC provider
+class BlocProvider<T extends BlocBase> extends StatefulWidget {
+  BlocProvider({
+    Key key,
+    @required this.child,
+    @required this.bloc,
+  }) : super(key: key);
+
+  final T bloc;
+  final Widget child;
+
+  _BlocProviderState<T> createState() => _BlocProviderState<T>();
+
+  static T of<T extends BlocBase>(BuildContext context) {
+    final type = _typeOf<BlocProvider<T>>();
+    BlocProvider<T> provider = context.ancestorWidgetOfExactType(type);
+    return provider.bloc;
+  }
+
+  static Type _typeOf<T>() => T;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
+
+class _BlocProviderState<T> extends State<BlocProvider<BlocBase>> {
+  @override
+  void dispose() {
+    widget.bloc.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return widget.child;
+  }
+}
