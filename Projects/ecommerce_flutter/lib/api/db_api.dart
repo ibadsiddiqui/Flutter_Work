017479059b58d2d9cbbabdd8e9e51e95faccd7f5
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-17 00:05:19 +0500

    --Commit: DB API added

---
+++ Projects/ecommerce_flutter/lib/api/db_api.dart
@@ -0,0 +1,8 @@
+import 'package:ecommerce_flutter/models/Category.dart';
+
+class DbAPI {
+  List<Category> getCategory() {
+    List<Category> tempList = [Category(), Category(), Category(), Category()];
+    return tempList;
+  }
+}
