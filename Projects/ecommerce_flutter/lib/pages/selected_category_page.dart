30d58748de5ce9865cd8ce336f7bec267667d9ad
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-18 00:14:08 +0500

    --Commit: selected category page created

---
+++ Projects/ecommerce_flutter/lib/pages/selected_category_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SelectedCategoryPage extends StatefulWidget {
+  SelectedCategoryPage({Key key}) : super(key: key);
+
+  _SelectedCategoryPageState createState() => _SelectedCategoryPageState();
+}
+
+class _SelectedCategoryPageState extends State<SelectedCategoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
