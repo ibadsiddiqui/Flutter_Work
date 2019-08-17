d9323ae4d270fb30d0d4cea9418e47f3d852dd1b
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-17 22:51:46 +0500

    --Commit: Stream controller added for Categories BLoC

--- Projects/ecommerce_flutter/lib/blocs/category_bloc.dart
+++ Projects/ecommerce_flutter/lib/blocs/category_bloc.dart
@@ -1,3 +1,19 @@
-class CategoriesBloc {
-  
+import 'dart:async';
+
+import 'package:ecommerce_flutter/blocprovs/bloc_provider.dart';
+import 'package:ecommerce_flutter/models/Category.dart';
+
+class CategoriesBloc implements BlocBase {
+  List<Category> categoryList;
+
+  StreamController<List<Category>> _categoriesController =
+      StreamController<List<Category>>();
+
+  Sink<List<Category>> get _intCategories => _categoriesController.sink;
+  Stream<List<Category>> get outCategories => _categoriesController.stream;
+
+  @override
+  void dispose() {
+    _categoriesController.close();
+  }
 }
