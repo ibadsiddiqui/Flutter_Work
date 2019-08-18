c359d4067526102b924e961325f99d3de56a85f3
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-18 09:21:03 +0500

    --Commit: Temp products added

--- Projects/ecommerce_flutter/lib/api/db_api.dart
+++ Projects/ecommerce_flutter/lib/api/db_api.dart
@@ -1,8 +1,19 @@
 import 'package:ecommerce_flutter/models/Category.dart';
+import 'package:ecommerce_flutter/models/Product.dart';
 
 class DbAPI {
   List<Category> getCategories() {
     List<Category> tempList = [Category(), Category(), Category(), Category()];
+    return tempList;
+  }
+
+  List<Product> getProducts(Category category) {
+    List<Product> tempList = [
+      Product.create("product"),
+      Product.create("product"),
+      Product.create("product"),
+      Product.create("product")
+    ];
     return tempList;
   }
 }
