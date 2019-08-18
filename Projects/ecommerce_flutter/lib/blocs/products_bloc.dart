23dc9c6891f042ef8d2228d6f9f5cfa197b077dd
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-18 09:20:57 +0500

    --Commit: Product BLoC created

---
+++ Projects/ecommerce_flutter/lib/blocs/products_bloc.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:ecommerce_flutter/api/db_api.dart';
+import 'package:ecommerce_flutter/blocprovs/bloc_provider.dart';
+import 'package:ecommerce_flutter/models/Category.dart';
+import 'package:ecommerce_flutter/models/Product.dart';
+
+class ProductsBloc implements BlocBase {
+  List<Product> _products;
+
+  StreamController<List<Product>> _productsController =
+      StreamController<List<Product>>();
+  Sink<List<Product>> get _intProducts => _productsController.sink;
+  Stream<List<Product>> get outProducts => _productsController.stream;
+
+  ProductsBloc(Category category) {
+    getProducts(category);
+  }
+
+  @override
+  void dispose() {
+    _productsController.close();
+  }
+
+  void getProducts(Category category) async {
+    DbAPI dbAPI = DbAPI();
+    _products = await dbAPI.getProducts(category);
+    _intProducts.add(_products);
+  }
+}
