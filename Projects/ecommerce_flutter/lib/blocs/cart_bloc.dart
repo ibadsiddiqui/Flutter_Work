aa9ffcb9f79150947deaa66e5d178df41fd16c70
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-25 01:10:42 +0500

    --Commit: Cart BLoC completed

--- Projects/ecommerce_flutter/lib/blocs/cart_bloc.dart
+++ Projects/ecommerce_flutter/lib/blocs/cart_bloc.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:ecommerce_flutter/blocprovs/bloc_provider.dart';
 import 'package:ecommerce_flutter/models/Cart.dart';
 import 'package:ecommerce_flutter/models/Product.dart';
+import 'package:rxdart/rxdart.dart';
 
 class CartBloc implements BlocBase {
   Cart _cart = Cart();
@@ -11,8 +12,13 @@ class CartBloc implements BlocBase {
   Sink<List<Product>> get _intProducts => _productController.sink;
   Stream<List<Product>> get outProducts => _productController.stream;
 
+  final _countController = BehaviorSubject<int>();
+  Sink<int> get _intCount => _countController.sink;
+  Stream<int> get outCount => _countController.stream;
+
   @override
   void dispose() {
     _productController.close();
+    _countController.close();
   }
 }
