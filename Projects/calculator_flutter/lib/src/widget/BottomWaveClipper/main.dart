68d8e5a94e9c1e835786afa40215c5d1ef579962
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-25 00:21:06 +0500

    --Commit: typo fixed

--- Projects/calculator_flutter/lib/src/widget/BottomWaveClipper/main.dart
+++ Projects/calculator_flutter/lib/src/widget/BottomWaveClipper/main.dart
@@ -13,7 +13,7 @@ class BottomWaveClipper extends CustomClipper<Path> {
 
     var secondControlPoint =
         Offset(size.width - (size.width / 3.25), size.height - 65);
-    var secondEndPoint = Offset(size.width, size.height - 40.0);
+    var secondEndPoint = Offset(size.width, size.height - 40);
     path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
         secondEndPoint.dx, secondEndPoint.dy);
 
