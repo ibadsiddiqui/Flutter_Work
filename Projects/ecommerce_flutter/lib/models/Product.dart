5a0d8b5d2d19ae0b3b3f147cf8c5aa3eca58e8e1
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-09-08 19:49:43 +0500

    --Commit: Product updated to handle json

--- Projects/ecommerce_flutter/lib/models/Product.dart
+++ Projects/ecommerce_flutter/lib/models/Product.dart
@@ -1,9 +1,15 @@
 class Product {
+  static const NAME_KEY = "name";
+
   String name;
   String id;
   int amount;
 
   Product.create(this.name);
+
+  Product.fromFirestore(Map<String, dynamic> json) {
+    name = json[NAME_KEY];
+  }
 
   bool operator(o) => o is Product && o.name == this.name && o.id == id;
 
