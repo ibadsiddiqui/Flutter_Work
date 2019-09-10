56de4ab0b283864bab6246093039893a3e1dafc3
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-09-10 21:07:04 +0500

    --commit: refactored

--- Projects/recipe_app/lib/pages/login.dart
+++ Projects/recipe_app/lib/pages/login.dart
@@ -1,33 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:recipe_app/widgets/backgroundImage/BackgroundImage.dart';
+import 'package:recipe_app/widgets/heading/HeadingWidget.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Text _buildText() {
-      return Text(
-        'Recipes',
-        textAlign: TextAlign.center,
-      );
-    }
-
     return Scaffold(
       backgroundColor: Colors.lightBlue,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _buildText(),
-            // Space between "Recipes" and the button:
-            SizedBox(height: 50.0),
-            MaterialButton(
-              color: Colors.white,
-              child: Text("Sign In with Google"),
-              onPressed: () => print("Button pressed."),
-            )
-          ],
+      body: Container(
+        // We do not use backgroundColor property anymore.
+        // New Container widget wraps our Center widget:
+        decoration: buildBackground("assets/brooke-lark-385507-unsplash.jpg"),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              buildText("Recipes", context),
+              // Space between "Recipes" and the button:
+              SizedBox(height: 50.0),
+              MaterialButton(
+                color: Colors.white,
+                child: Text("Sign In with Google"),
+                onPressed: () => print("Button pressed."),
+              )
+            ],
+          ),
         ),
       ),
     );
