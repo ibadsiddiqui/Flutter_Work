1ad2ceaf25fe893e666521980d351b9205e317ce
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-09-10 21:44:09 +0500

    --Commit: font updated

--- Projects/recipe_app/lib/widgets/GoogleSignInButton/google_sign_in_button.dart
+++ Projects/recipe_app/lib/widgets/GoogleSignInButton/google_sign_in_button.dart
@@ -1,37 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class GoogleSignInButton extends StatelessWidget {
   GoogleSignInButton({this.onPressed});
 
   final Function onPressed;
 
-  Image _buildLogo() {
-    return Image.asset(
-      "assets/g-logo.png",
-      height: 18.0,
-      width: 18.0,
-    );
-  }
-
-  Opacity _buildText() {
-    return Opacity(
-      opacity: 0.54,
-      child: Text(
-        "Sign in with Google",
-        style: TextStyle(
-          fontFamily: 'Roboto-Medium',
-          color: Colors.black,
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
+    Image _buildLogo() {
+      return Image.asset(
+        "assets/g-logo.png",
+        height: 18.0,
+        width: 18.0,
+      );
+    }
+
+    Opacity _buildText() {
+      return Opacity(
+        opacity: 0.54,
+        child: Text(
+          "Sign in with Google",
+          style: TextStyle(
+            fontFamily: 'Roboto-Medium',
+            color: Colors.black,
+          ),
+        ),
+      );
+    }
+
     return MaterialButton(
-      onPressed: onPressed(),
       height: 40.0,
+      onPressed: this.onPressed,
       color: Colors.white,
       child: Row(
         mainAxisSize: MainAxisSize.min,
