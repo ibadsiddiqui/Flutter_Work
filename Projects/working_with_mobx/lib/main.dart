c726685222dba95f605eb06379cad92e0ed45651
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-10-03 15:23:52 +0500

    --Commit: todo app added

--- Projects/working_with_mobx/lib/main.dart
+++ Projects/working_with_mobx/lib/main.dart
@@ -1,53 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
-import 'counter.dart'; // Import the Counter
+// import 'package:working_with_mobx/counter_app/screens/CounterScreen.dart';
+import 'package:working_with_mobx/todos/screen/TodosList.dart';
 
-final counter = Counter();
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Mobx App"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Observer(
-              builder: (_) => Text(
-                '${counter.value}',
-                style: Theme.of(context).textTheme.display1,
-              ),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: counter.increment,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
+// void main() => runApp(MyApp());
+void main() => runApp(MyTodoApp());
