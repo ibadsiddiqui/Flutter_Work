d50042d27a8175fb56a9a0178026bf6f097782ef
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-10-08 22:37:41 +0500

    --Commit: listener added and mapped to products list

--- Projects/ecommerce_flutter/lib/blocs/products_bloc.dart
+++ Projects/ecommerce_flutter/lib/blocs/products_bloc.dart
@@ -1,12 +1,13 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerce_flutter/api/db_api.dart';
 import 'package:ecommerce_flutter/blocprovs/bloc_provider.dart';
 import 'package:ecommerce_flutter/models/Category.dart';
 import 'package:ecommerce_flutter/models/Product.dart';
 
 class ProductsBloc implements BlocBase {
-  List<Product> _products;
+  List<Product> _products = List();
 
   StreamController<List<Product>> _productsController =
       StreamController<List<Product>>();
@@ -24,7 +25,16 @@ class ProductsBloc implements BlocBase {
 
   void getProducts(Category category) {
     DbAPI dbAPI = DbAPI();
-    _products = dbAPI.getProducts() as List<Product>;
-    _intProducts.add(_products);
+    dbAPI.getProducts(category).listen((snapshot) {
+      List<Product> tempProducts = List();
+      for (DocumentSnapshot doc in snapshot.documents) {
+        Product product = Product.fromFirestore(doc.data);
+        product.id = doc.documentID;
+        tempProducts.add(product);
+      }
+      _products.clear();
+      _products.addAll(tempProducts);
+      _intProducts.add(_products);
+    });
   }
 }
