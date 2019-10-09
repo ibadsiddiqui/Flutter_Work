9f9c04a422b4d13ff7e88ba9489e66a15b1c9a93
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-10-09 15:21:10 +0500

    --Commit: sign up button added to login

--- Projects/ever_events/lib/src/screens/login_screen/login.dart
+++ Projects/ever_events/lib/src/screens/login_screen/login.dart
@@ -1,3 +1,5 @@
+import 'dart:ui' as prefix0;
+
 import 'package:ever_events/src/widgets/auth/Inputs.dart';
 import 'package:flutter/material.dart';
 
@@ -11,63 +13,71 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new Container(
+        padding: EdgeInsets.only(top: 150),
         decoration: new BoxDecoration(
           image: new DecorationImage(
             image: AssetImage("asset/images/Auth_BG.jpg"),
             fit: BoxFit.cover,
           ),
         ),
-        alignment: Alignment.center,
-        child: new Center(
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Text(
-                "Ever Events",
-                style: Theme.of(context).textTheme.headline,
-              ),
-              new Padding(
+        child: new Column(
+          children: <Widget>[
+            new Text("Ever Events",
+                style: Theme.of(context).textTheme.headline),
+            new Padding(
                 padding: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
-                child: authInput("Username", (String value) {}, (String value) {
-                  print(value);
-                }),
-              ),
-              new Padding(
+                child: authInput(
+                    label: "Username",
+                    validator: (String value) {},
+                    handleChange: (String value) {
+                      print(value);
+                    })),
+            new Padding(
                 padding: EdgeInsets.only(top: 15.0, left: 20.0, right: 20.0),
-                child: authInput("Email", (String value) {}, (String value) {
-                  print(value);
-                }),
+                child: authInput(
+                  label: "Password",
+                  validator: (String value) {},
+                  handleChange: (String value) {},
+                  obsure: true,
+                )),
+            const SizedBox(height: 30),
+            new RaisedButton(
+              onPressed: () {},
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: const <Widget>[
+                  SizedBox(width: 100.0, height: 57.5),
+                  Text("SIGN IN",
+                      maxLines: 1,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      )),
+                  SizedBox(width: 100.0, height: 57.5),
+                ],
               ),
-              new Padding(
-                padding: EdgeInsets.only(top: 15.0, left: 20.0, right: 20.0),
-                child: authInput("Password", (String value) {}, (String value) {
-                  print(value);
-                }),
-              ),
-              const SizedBox(height: 30),
-              new RaisedButton(
-                onPressed: () {},
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: const <Widget>[
-                    SizedBox(width: 100.0, height: 50.0,),
-                    Text("SIGN UP",
-                        maxLines: 1,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20.0,
-                        )),
-                    SizedBox(width: 100.0, height: 50.0,),
-                  ],
+              textColor: Colors.white,
+              padding: EdgeInsets.only(left: 20.0, right: 20.0),
+              color: Colors.pinkAccent,
+              splashColor: Colors.pink,
+              shape: const StadiumBorder(),
+            ),
+            Container(
+              height: 300.0,
+              alignment: Alignment.bottomCenter,
+              child: FlatButton(
+                onPressed: () => Navigator.of(context).pushReplacementNamed("/SignUp"),
+                child: Text(
+                  "New here? SIGN UP",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-                textColor: Colors.white,
-                padding: EdgeInsets.only(left: 20.0, right: 20.0),
-                color: Colors.pinkAccent,
-                splashColor: Colors.pink,
-                shape: const StadiumBorder(),
-              )
-            ],
-          ),
+              ),
+            )
+          ],
         ),
       ),
     );
