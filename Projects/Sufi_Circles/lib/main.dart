57f0a72696b29b239d18167fae8ddfcafa9198b0
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-10-11 16:25:39 +0500

    --Commit: pub spec updated

--- Projects/Sufi_Circles/lib/main.dart
+++ Projects/Sufi_Circles/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:Sufi_Circles/src/pages/login_screen/login.dart';
 import 'package:Sufi_Circles/src/pages/signup_screen/signup.dart';
 import 'package:Sufi_Circles/src/pages/splash_screen/splash.dart';
-import 'package:Sufi_Circles/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(EverEventsApp());
@@ -12,7 +11,6 @@ class EverEventsApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
-      theme: buildTheme(),
       routes: <String, WidgetBuilder>{
         // If you're using navigation routes, Flutter needs a base route.
         // We're going to change this route once we're ready with
