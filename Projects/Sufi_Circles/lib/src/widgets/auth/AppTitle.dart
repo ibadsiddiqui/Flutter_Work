04e8c548c75b90423a996c60895db8205845e786
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-10-14 11:22:51 +0500

    --Commit: App Title Widget created

---
+++ Projects/Sufi_Circles/lib/src/widgets/auth/AppTitle.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Text(
+      "Ever Events",
+      style: TextStyle(
+        fontSize: 40,
+        fontFamily: "CreteRound",
+      ),
+    );
+  }
+}
