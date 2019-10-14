92541a26104d10c2faf1da0b4faa5c546d8c1d05
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-10-14 12:43:53 +0500

    --Commit: Multiprovider added to main

--- Projects/Sufi_Circles/lib/main.dart
+++ Projects/Sufi_Circles/lib/main.dart
@@ -1,25 +1,32 @@
+import 'package:Sufi_Circles/src/models/auth/AuthFormModel.dart';
 import 'package:Sufi_Circles/src/pages/login_screen/login.dart';
 import 'package:Sufi_Circles/src/pages/signup_screen/signup.dart';
 import 'package:Sufi_Circles/src/pages/splash_screen/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(SufiCirlces());
+void main() => runApp(App());
 
-class SufiCirlces extends StatelessWidget {
+class App extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        // If you're using navigation routes, Flutter needs a base route.
-        // We're going to change this route once we're ready with
-        // implementation of HomeScreen.
-        // "/splash": (context) => SplashScreen(),
-        '/': (context) => SplashScreen(),
-        '/LoginScreen': (context) => LoginScreen(),
-        '/SignUpScreen': (context) => SignUpScreen(),
-      },
-    );
-  }
+  Widget build(BuildContext context) => MultiProvider(
+        providers: [
+          Provider<AuthModel>(
+            builder: (_) => AuthModel(),
+          )
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          initialRoute: '/',
+          routes: <String, WidgetBuilder>{
+            // If you're using navigation routes, Flutter needs a base route.
+            // We're going to change this route once we're ready with
+            // implementation of HomeScreen.
+            // "/splash": (context) => SplashScreen(),
+            '/': (context) => SplashScreen(),
+            '/LoginScreen': (context) => LoginScreen(),
+            '/SignUpScreen': (context) => SignUpScreen(),
+          },
+        ),
+      );
 }
