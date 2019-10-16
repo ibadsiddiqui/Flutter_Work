e88e8b8bb7b1f66126c9fbd0ac7bda1d3f827822
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-10-16 18:21:09 +0500

    --Commit: styling updated

--- Projects/Sufi_Circles/lib/src/widgets/auth/BottomButton.dart
+++ Projects/Sufi_Circles/lib/src/widgets/auth/BottomButton.dart
@@ -4,19 +4,32 @@ class BottomButton extends StatelessWidget {
   final Function onPressed;
   final String title;
   const BottomButton({Key key, this.onPressed, this.title}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        alignment: Alignment.bottomCenter,
-        child: FlatButton(
-          onPressed: () => onPressed(context),
-          child: Text(title,
-              style: TextStyle(
-                fontFamily: "Comfortaa",
-                color: Colors.black,
-                fontSize: 18.5,
+    return new Container(
+        width: MediaQuery.of(context).size.width,
+        margin: const EdgeInsets.only(
+            left: 40.0, right: 40.0, top: 10.0, bottom: 20.0),
+        alignment: Alignment.center,
+        child: new Row(
+          children: <Widget>[
+            new Expanded(
+              child: new FlatButton(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 20.0, horizontal: 20.0),
+                color: Colors.transparent,
+                onPressed: () => onPressed(context),
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: "Comfortaa",
+                  ),
+                ),
               ),
-              textAlign: TextAlign.center),
+            ),
+          ],
         ));
   }
 }
--- Projects/Sufi_Circles/lib/src/widgets/auth/SubmitButton.dart
+++ Projects/Sufi_Circles/lib/src/widgets/auth/SubmitButton.dart
@@ -4,30 +4,34 @@ class SubmitButton extends StatelessWidget {
   const SubmitButton({Key key, this.title, this.onPressed}) : super(key: key);
   final String title;
   final Function onPressed;
+
   @override
   Widget build(BuildContext context) {
-    return new RaisedButton(
-      onPressed: this.onPressed,
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
+    return new Container(
+      width: MediaQuery.of(context).size.width,
+      margin: const EdgeInsets.only(left: 40.0, right: 40.0, top: 30.0),
+      alignment: Alignment.center,
+      child: new Row(
         children: <Widget>[
-          SizedBox(width: 100.0, height: 57.5),
-          Text(
-            this.title,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20.0,
-              fontFamily: "CreteRound",
+          new Expanded(
+            child: new ClipRRect(
+              borderRadius: new BorderRadius.circular(8.0),
+              child: new FlatButton(
+                color: Color.fromRGBO(8, 51, 88, 1),
+                splashColor: Color.fromRGBO(7, 34, 71, 1),
+                padding: const EdgeInsets.symmetric(
+                    vertical: 20.0, horizontal: 20.0),
+                onPressed: onPressed,
+                child: Text(
+                  this.title,
+                  style:
+                      TextStyle(color: Colors.white, fontFamily: "Comfortaa"),
+                ),
+              ),
             ),
           ),
-          SizedBox(width: 100.0, height: 57.5),
         ],
       ),
-      textColor: Colors.white,
-      padding: EdgeInsets.only(left: 20.0, right: 20.0),
-      color: Color.fromRGBO(8, 51, 88, 1),
-      splashColor: Color.fromRGBO(7, 34, 71, 1),
-      shape: StadiumBorder(),
     );
   }
 }
