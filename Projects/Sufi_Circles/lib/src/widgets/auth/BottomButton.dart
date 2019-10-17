533f95fb151f2845ba69c1022ce6683fedf7cbc4
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-10-17 14:23:08 +0500

    --Commit: padding fixed for bottom text for large screen

--- Projects/Sufi_Circles/lib/src/widgets/auth/BottomButton.dart
+++ Projects/Sufi_Circles/lib/src/widgets/auth/BottomButton.dart
@@ -8,28 +8,25 @@ class BottomButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Container(
-        width: MediaQuery.of(context).size.width,
-        margin: const EdgeInsets.only(
-            left: 40.0, right: 40.0, top: 10.0, bottom: 20.0),
-        alignment: Alignment.center,
-        child: new Row(
-          children: <Widget>[
-            new Expanded(
-              child: new FlatButton(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 20.0, horizontal: 20.0),
-                color: Colors.transparent,
-                onPressed: () => onPressed(context),
-                child: Text(
-                  title,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: "Comfortaa",
-                  ),
-                ),
+      width: MediaQuery.of(context).size.width,
+      margin: const EdgeInsets.only(
+          left: 40.0, right: 40.0, top: 10.0, bottom: 20.0),
+      alignment: Alignment.center,
+      child: new Row(
+        children: <Widget>[
+          new Expanded(
+            child: new FlatButton(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              color: Colors.transparent,
+              onPressed: () => onPressed(context),
+              child: Text(
+                title,
+                style: TextStyle(color: Colors.white, fontFamily: "Comfortaa"),
               ),
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
