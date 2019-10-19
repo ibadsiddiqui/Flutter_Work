64d0a1872996104e1982c5716da59ef44570fc67
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-10-19 22:46:19 +0500

    --Commit: navigation added for routes

--- Projects/Sufi_Circles/lib/src/navigator/auth_navigator.dart
+++ Projects/Sufi_Circles/lib/src/navigator/auth_navigator.dart
@@ -1,5 +1,24 @@
 import 'package:flutter/material.dart';
 
+Route createRoute({Widget screen}) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => screen,
+    transitionDuration: Duration(milliseconds: 500),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(1.0, 0.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
+
 void pushLoginScreen(context) {
   Navigator.of(context).pushReplacementNamed("/LoginScreen");
 }
