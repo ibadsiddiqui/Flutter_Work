313f03966362f20bf98b27ce6f35c5698e8e2021
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-10-19 09:10:35 +0500

    --Commit: overlays removed

--- Projects/Sufi_Circles/lib/main.dart
+++ Projects/Sufi_Circles/lib/main.dart
@@ -6,30 +6,34 @@ import 'src/pages/login_screen/login.dart';
 import 'src/pages/onboarding_screen/onboarding.dart';
 import 'src/pages/signup_screen/signup.dart';
 import 'src/pages/splash_screen/splash.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(App());
 
 class App extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => MultiProvider(
-        providers: [
-          Provider<AuthModel>(
-            builder: (_) => AuthModel(),
-          )
-        ],
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          initialRoute: '/',
-          routes: <String, WidgetBuilder>{
-            // If you're using navigation routes, Flutter needs a base route.
-            // We're going to change this route once we're ready with
-            // implementation of HomeScreen.
-            // "/splash": (context) => SplashScreen(),
-            '/': (context) => SplashScreen(),
-            '/OnBoardingScreen': (context) => OnBoardingScreen(),
-            '/LoginScreen': (context) => LoginScreen(),
-            '/SignUpScreen': (context) => SignUpScreen(),
-          },
-        ),
-      );
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    return MultiProvider(
+      providers: [
+        Provider<AuthModel>(
+          builder: (_) => AuthModel(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: <String, WidgetBuilder>{
+          // If you're using navigation routes, Flutter needs a base route.
+          // We're going to change this route once we're ready with
+          // implementation of HomeScreen.
+          // "/splash": (context) => SplashScreen(),
+          '/': (context) => SplashScreen(),
+          '/OnBoardingScreen': (context) => OnBoardingScreen(),
+          '/LoginScreen': (context) => LoginScreen(),
+          '/SignUpScreen': (context) => SignUpScreen(),
+        },
+      ),
+    );
+  }
 }
