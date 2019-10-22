b87767169e64d16ce47da459d05eb80bc534bff2
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-10-23 00:58:26 +0500

    --Commit: list added for dashboard

--- Projects/Sufi_Circles/lib/src/pages/dashboard_screen/dashboard.dart
+++ Projects/Sufi_Circles/lib/src/pages/dashboard_screen/dashboard.dart
@@ -1,5 +1,8 @@
 import 'package:Sufi_Circles/src/widgets/dashboard/background.dart';
 import 'package:flutter/material.dart';
+import 'package:Sufi_Circles/src/widgets/dashboard/appbar.dart';
+import 'package:Sufi_Circles/src/widgets/dashboard/heading.dart';
+import 'package:Sufi_Circles/src/widgets/dashboard/top_tile.dart';
 
 class DashboardScreen extends StatefulWidget {
   @override
@@ -15,35 +18,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: Drawer(),
-      appBar: AppBar(
-        centerTitle: true,
-        elevation: 10.0,
-        backgroundColor: Color(0xFF072247),
-        title: !isSearching
-            ? TextFormField(
-                autofocus: true,
-                decoration: InputDecoration.collapsed(
-                  hintText: "Search...",
-                  hintStyle: TextStyle(color: Colors.white),
-                ),
-                strutStyle: StrutStyle(),
-                textAlign: TextAlign.center,
-                cursorColor: Colors.white,
-                style: TextStyle(color: Colors.white, fontFamily: "Comfortaa"),
-              )
-            : const Text(
-                'Sufi Circles',
-                style: TextStyle(fontFamily: "CreteRound"),
-                textAlign: TextAlign.center,
-              ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search, color: Colors.white),
-            onPressed: toggleSearch,
-          ),
-          SizedBox(height: 10, width: 10)
-        ],
-      ),
+      appBar: dashboardAppBar(isSearching: isSearching, onPress: toggleSearch),
       body: Container(
         child: ListView(
           children: <Widget>[
@@ -56,115 +31,132 @@ class _DashboardScreenState extends State<DashboardScreen> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Container(
-                          decoration: dashboardTopTabBackground(),
-                          width: 175,
-                          height: 175,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Text("Come and Discover the cream of the crop",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: "Comfortaa",
-                                  )),
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(50),
-                                  child: FlatButton(
-                                    // color: Color(0xFF072247),
-                                    color: Colors.redAccent,
-                                    onPressed: () {},
-                                    child: Text(
-                                      "Book Event",
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontFamily: "CreteRound",
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Container(
-                          decoration: dashboardTopTabBackground(),
-                          width: 175,
-                          height: 175,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Text(
-                                "Come and Discover the cream of the crop",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(color: Colors.white),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
+                      DashboardTopTile(),
+                      DashboardTopTile(),
                     ],
                   ),
                   SizedBox(height: 10.0),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Container(
-                          decoration: dashboardTopTabBackground(),
-                          width: 175,
-                          height: 175,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Text(
-                                "Come and Discover the cream of the crop",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(color: Colors.white),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Container(
-                          decoration: dashboardTopTabBackground(),
-                          width: 175,
-                          height: 175,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Text(
-                                "Come and Discover the cream of the crop",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(color: Colors.white),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
+                      DashboardTopTile(),
+                      DashboardTopTile(),
                     ],
                   ),
                   Padding(
                     padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: Text(
-                      "Latest Events",
-                      style: TextStyle(
-                        fontFamily: "CreteRound",
-                        color: Colors.black,
-                        fontSize: 22,
+                    child: DashboardHeadings(title: "Latest Events"),
+                  ),
+                  SizedBox(height: 10.0),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10.0),
+                        child: FlatButton(
+                          onPressed: () {},
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10.0),
+                            child: Container(
+                              padding: EdgeInsets.all(10.0),
+                              width: MediaQuery.of(context).size.width * 0.92,
+                              height:
+                                  MediaQuery.of(context).size.height * 0.225,
+                              decoration: dashboardTopTabBackground(),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Container(
+                                    alignment: Alignment.center,
+                                    width: "Nia Night Music Rapper"
+                                            .length
+                                            .toDouble() *
+                                        20,
+                                    child: Text(
+                                      "Nia Night Music Rapper",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 22.0,
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    width: "Nia Night Music Rapper"
+                                            .length
+                                            .toDouble() *
+                                        10,
+                                    child: Divider(
+                                      color: Color(0xFFFC3C3C),
+                                      thickness: 2,
+                                      height: 1,
+                                    ),
+                                  ),
+                                  Text(
+                                    "asdjnasldnasdasildn asdnalsd asld nasnd as dasldaslk Nia Night Music Rapper",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      // fontSize: 22.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
+                      FlatButton(
+                        onPressed: () {},
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10.0),
+                          child: Container(
+                            padding: EdgeInsets.all(10.0),
+                            width: MediaQuery.of(context).size.width * 0.92,
+                            height: MediaQuery.of(context).size.height * 0.225,
+                            decoration: dashboardTopTabBackground(),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Container(
+                                  alignment: Alignment.center,
+                                  width: "Nia Night Music Rapper"
+                                          .length
+                                          .toDouble() *
+                                      20,
+                                  child: Text(
+                                    "Nia Night Music Rapper",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 22.0,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  width: "Nia Night Music Rapper"
+                                          .length
+                                          .toDouble() *
+                                      10,
+                                  child: Divider(
+                                    color: Color(0xFFFC3C3C),
+                                    thickness: 2,
+                                    height: 1,
+                                  ),
+                                ),
+                                Text(
+                                  "asdjnasldnasdasildn asdnalsd asld nasnd as dasldaslk Nia Night Music Rapper",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    // fontSize: 22.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   )
                 ],
               ),
