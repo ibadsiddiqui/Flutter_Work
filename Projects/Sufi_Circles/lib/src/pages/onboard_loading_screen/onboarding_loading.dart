7c9b784b26c27a1e4cce7b723b337657194596c4
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-10-29 00:22:40 +0500

    --Commit: keys added for local storage and navigation updated for already installed app

--- Projects/Sufi_Circles/lib/src/pages/onboard_loading_screen/onboarding_loading.dart
+++ Projects/Sufi_Circles/lib/src/pages/onboard_loading_screen/onboarding_loading.dart
@@ -1,5 +1,8 @@
+import 'package:Sufi_Circles/src/constants/keys.dart';
+import 'package:Sufi_Circles/src/pages/login_screen/login.dart';
 import 'package:Sufi_Circles/src/pages/onboarding_screen/onboarding.dart';
 import 'package:Sufi_Circles/src/navigator/timed_navigation.dart';
+import 'package:Sufi_Circles/src/utils/share_utils.dart';
 import 'package:Sufi_Circles/src/widgets/loader/dot_type.dart';
 import 'package:Sufi_Circles/src/widgets/loader/loader.dart';
 import 'package:flutter/material.dart';
@@ -11,10 +14,26 @@ class OnBoardingLoadingScreen extends StatefulWidget {
 }
 
 class _OnBoardingLoadingScreenState extends State<OnBoardingLoadingScreen> {
-  @override
-  void initState() {
-    super.initState();
-    TimeNavigation.navigate(context, OnBoardingScreen());
+  ShareUtils utils = ShareUtils();
+
+  @protected
+  @mustCallSuper
+  Future didChangeDependencies() async => navigate();
+
+  void navigate() async {
+    try {
+      bool isAppInstalled = await utils.getBoolPreference("isInstalled");
+      print(isAppInstalled);
+      if (isAppInstalled != null && isAppInstalled == true)
+        TimeNavigation.navigate(context, LoginScreen());
+      else {
+        utils.setBoolPreference("isInstalled", true);
+        TimeNavigation.navigate(context, OnBoardingScreen());
+      }
+    } on NoSuchMethodError catch (e) {
+      utils.setBoolPreference(IsInstalled, true);
+      TimeNavigation.navigate(context, OnBoardingScreen());
+    }
   }
 
   @override
