932ece3f7cb34437876f52f2b168210cbe5043b9
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-10-29 00:17:10 +0500

    --Commit: shared preferences uupdated

--- Projects/Sufi_Circles/lib/src/utils/share_utils.dart
+++ Projects/Sufi_Circles/lib/src/utils/share_utils.dart
@@ -2,29 +2,24 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:async';
 
 class ShareUtils {
-  SharedPreferences prefs;
-
-  ShareUtils() {
-    getInstance();
-  }
-
-  void getInstance() async {
-    prefs = await SharedPreferences.getInstance();
-  }
 
   Future<bool> setBoolPreference(key, value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.setBool(key, value);
   }
 
   Future<bool> getBoolPreference(key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.getBool(key);
   }
 
   Future<bool> setStringPreference(key, value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.setString(key, value);
   }
 
   Future<String> getStringPreference(key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.getString(key);
   }
 }
