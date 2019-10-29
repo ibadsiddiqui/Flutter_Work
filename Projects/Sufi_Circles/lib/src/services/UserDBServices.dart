9410898551099c1d362c8e837966cca2b7fe8d25
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-10-29 16:44:39 +0500

    --Commit: file updated

--- Projects/Sufi_Circles/lib/src/services/UserDBServices.dart
+++ Projects/Sufi_Circles/lib/src/services/UserDBServices.dart
@@ -1,12 +1,22 @@
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class UserServices {
+class UserDBServices {
   CollectionReference userDB = Firestore.instance.collection("user");
 
   Future<void> createUserInDB(Map<String, dynamic> user) async {
     try {
       return (await userDB.document(user["uid"]).setData(user));
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<void> updateUserLastLogin(Map<String, dynamic> user) async {
+    try {
+      return (await userDB
+          .document(user["uid"])
+          .updateData({"lastSignInTime": user["lastSignInTime"]}));
     } catch (e) {
       throw e;
     }
