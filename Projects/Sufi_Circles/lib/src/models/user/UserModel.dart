70b5a1d82c6144b5d43b16941e81e9795fc4154b
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-10-30 17:11:37 +0500

    --Commit: user model updated

--- Projects/Sufi_Circles/lib/src/models/user/UserModel.dart
+++ Projects/Sufi_Circles/lib/src/models/user/UserModel.dart
@@ -6,14 +6,33 @@ class UserModel = _UserModel with _$UserModel;
 
 abstract class _UserModel with Store {
   @observable
+  String name = '';
+
+  @observable
   String email = '';
 
   @observable
   bool isEmailVerified = false;
+
+  @observable
+  String country = '';
+
+  @observable
+  String city = '';
+
+  @action
+  void setUserName(String name) => this.name = name;
 
   @action
   void setUserEmail(String email) => this.email = email;
 
   @action
   void setEmailVerificationStatus(bool status) => this.isEmailVerified = status;
+
+  @action
+  void setUserCountry(String country) => this.country = country;
+
+  @action
+  void setUserCity(String city) => this.city = city;
+
 }
--- Projects/Sufi_Circles/lib/src/models/user/UserModel.g.dart
+++ Projects/Sufi_Circles/lib/src/models/user/UserModel.g.dart
@@ -9,6 +9,23 @@ part of 'UserModel.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$UserModel on _UserModel, Store {
+  final _$nameAtom = Atom(name: '_UserModel.name');
+
+  @override
+  String get name {
+    _$nameAtom.context.enforceReadPolicy(_$nameAtom);
+    _$nameAtom.reportObserved();
+    return super.name;
+  }
+
+  @override
+  set name(String value) {
+    _$nameAtom.context.conditionallyRunInAction(() {
+      super.name = value;
+      _$nameAtom.reportChanged();
+    }, _$nameAtom, name: '${_$nameAtom.name}_set');
+  }
+
   final _$emailAtom = Atom(name: '_UserModel.email');
 
   @override
@@ -43,7 +60,51 @@ mixin _$UserModel on _UserModel, Store {
     }, _$isEmailVerifiedAtom, name: '${_$isEmailVerifiedAtom.name}_set');
   }
 
+  final _$countryAtom = Atom(name: '_UserModel.country');
+
+  @override
+  String get country {
+    _$countryAtom.context.enforceReadPolicy(_$countryAtom);
+    _$countryAtom.reportObserved();
+    return super.country;
+  }
+
+  @override
+  set country(String value) {
+    _$countryAtom.context.conditionallyRunInAction(() {
+      super.country = value;
+      _$countryAtom.reportChanged();
+    }, _$countryAtom, name: '${_$countryAtom.name}_set');
+  }
+
+  final _$cityAtom = Atom(name: '_UserModel.city');
+
+  @override
+  String get city {
+    _$cityAtom.context.enforceReadPolicy(_$cityAtom);
+    _$cityAtom.reportObserved();
+    return super.city;
+  }
+
+  @override
+  set city(String value) {
+    _$cityAtom.context.conditionallyRunInAction(() {
+      super.city = value;
+      _$cityAtom.reportChanged();
+    }, _$cityAtom, name: '${_$cityAtom.name}_set');
+  }
+
   final _$_UserModelActionController = ActionController(name: '_UserModel');
+
+  @override
+  void setUserName(String name) {
+    final _$actionInfo = _$_UserModelActionController.startAction();
+    try {
+      return super.setUserName(name);
+    } finally {
+      _$_UserModelActionController.endAction(_$actionInfo);
+    }
+  }
 
   @override
   void setUserEmail(String email) {
@@ -60,6 +121,26 @@ mixin _$UserModel on _UserModel, Store {
     final _$actionInfo = _$_UserModelActionController.startAction();
     try {
       return super.setEmailVerificationStatus(status);
+    } finally {
+      _$_UserModelActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setUserCountry(String country) {
+    final _$actionInfo = _$_UserModelActionController.startAction();
+    try {
+      return super.setUserCountry(country);
+    } finally {
+      _$_UserModelActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setUserCity(String city) {
+    final _$actionInfo = _$_UserModelActionController.startAction();
+    try {
+      return super.setUserCity(city);
     } finally {
       _$_UserModelActionController.endAction(_$actionInfo);
     }
