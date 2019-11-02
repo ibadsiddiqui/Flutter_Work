516df07cc8b255a5869b94538cad5934bd8c2ad6
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-11-03 01:25:39 +0500

    --commit: sign out from firebase added

--- Projects/Sufi_Circles/lib/src/services/api/AuthServices.dart
+++ Projects/Sufi_Circles/lib/src/services/api/AuthServices.dart
@@ -24,12 +24,29 @@ class AuthService {
     }
   }
 
+  Future<void> signOutUser() async {
+    try {
+      return (await _firebaseAuth.signOut());
+    } catch (e) {
+      print(e);
+    }
+  }
+
   Future<String> currentUser() async {
     FirebaseUser user = await _firebaseAuth.currentUser();
     return user != null ? user.uid : null;
   }
 
+  Future<void> updateFirebaseUserEmail(String email) async {
+    FirebaseUser user = await _firebaseAuth.currentUser();
+    await user.updateEmail(email);
+  }
+
   Future<void> signOut() async {
-    return _firebaseAuth.signOut();
+    try {
+      return _firebaseAuth.signOut();
+    } catch (e) {
+      print(e);
+    }
   }
 }
