a8eda5d6550e30eda859c1a6b9ef81b5ad40f5fc
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-11-03 11:13:59 +0500

    --Commit: file created for create event

---
+++ Projects/Sufi_Circles/lib/src/pages/create_event/create_event.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CreateEvent extends StatelessWidget {
+  const CreateEvent({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // child: child,
+      // appBar: ,
+      body: Container(child: Text('asdasd'),),
+    );
+  }
+}
