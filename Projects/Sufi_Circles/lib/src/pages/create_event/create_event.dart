bfbda459fd1947c9fd6f426ee97fba33807076f4
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-11-03 15:37:54 +0500

    --Commit; button added for click to start creating event

--- Projects/Sufi_Circles/lib/src/pages/create_event/create_event.dart
+++ Projects/Sufi_Circles/lib/src/pages/create_event/create_event.dart
@@ -1,3 +1,4 @@
+import 'package:Sufi_Circles/main.dart';
 import 'package:flutter/material.dart';
 
 class CreateEvent extends StatelessWidget {
@@ -6,9 +7,30 @@ class CreateEvent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // child: child,
-      // appBar: ,
-      body: Container(child: Text('asdasd'),),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        iconTheme: IconThemeData(
+          color: Color(0xFF072247),
+        ),
+        elevation: 0,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Text('Create something that people remember',
+                style: TextStyle(fontFamily: "Comfortaa")),
+          ),
+          FlatButton(
+            color: Color(0xFFFC3C3C),
+            onPressed: () {},
+            child: Text(
+              "Click here to start",
+              style: TextStyle(color: Colors.white, fontFamily: "Comfortaa"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
