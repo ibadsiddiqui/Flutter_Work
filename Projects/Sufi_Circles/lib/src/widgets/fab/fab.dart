296735bc79563a319d849f293b8f73e6224829b9
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-11-05 13:34:09 +0500

    --Commit: fab updated

---
+++ Projects/Sufi_Circles/lib/src/widgets/fab/fab.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class BottomFABs extends StatelessWidget {
+  final String toolTip1;
+  final String toolTip2;
+  final Function onPress2;
+  final Function onPress1;
+  final Icon icon1;
+  final Icon icon2;
+  const BottomFABs(
+      {Key key,
+      this.onPress2,
+      this.onPress1,
+      this.toolTip1,
+      this.toolTip2,
+      this.icon1,
+      this.icon2})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: FloatingActionButton(
+              backgroundColor: Color(0xFF072247),
+              heroTag: toolTip1,
+              tooltip: toolTip1,
+              child: icon1,
+              onPressed: onPress1,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: FloatingActionButton(
+              heroTag: toolTip2,
+              tooltip: toolTip2,
+              backgroundColor: Color(0xFF072247),
+              child: icon2,
+              onPressed: onPress2,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
