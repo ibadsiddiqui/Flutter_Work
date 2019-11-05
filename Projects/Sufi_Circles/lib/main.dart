503e706be44a8bee7f15345c70e318cfaf78d09c
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-11-05 13:34:01 +0500

    --Commit: display image page added

--- Projects/Sufi_Circles/lib/main.dart
+++ Projects/Sufi_Circles/lib/main.dart
@@ -20,7 +20,7 @@ void main() => runApp(App());
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIOverlays([]);
+    // SystemChrome.setEnabledSystemUIOverlays([]);
     return MultiProvider(
       providers: [
         Provider<AuthModel>(
