4ce72bbbf8bb25536060fefb899850c758a60dd4
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-11-11 20:49:43 +0500

    --Commit: event date selectors added

--- Projects/Sufi_Circles/lib/src/widgets/add_event_details/add_event_dates.dart
+++ Projects/Sufi_Circles/lib/src/widgets/add_event_details/add_event_dates.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:Sufi_Circles/src/models/event/EventModel.dart';
@@ -9,61 +11,159 @@ class AddEventDate extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print(Provider.of<EventModel>(context).dateFrom.value);
-
     return Container(
       padding: EdgeInsets.only(left: 2, right: 2),
-      child: Consumer<EventModel>(builder: (_, data, __) {
-        var parsedDateFrom = DateTime.parse(data.dateFrom.value);
-        var parsedDateTo = DateTime.parse(data.dateFrom.value);
-        var dateFromNew = new DateFormat('yyyy-MM-dd').format(parsedDateFrom);
-        var dateToNew = new DateFormat('yyyy-MM-dd').format(parsedDateTo);
-        // print(formatted);
-        return Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 18),
-              child: FormHeading(heading: "Add dates of the event."),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      "From:",
-                      style: TextStyle(fontSize: 20, color: Color(0xFF072247)),
+      child: Consumer<EventModel>(
+        builder: (_, data, __) {
+          var parsedDateFrom = DateTime.now();
+          var parsedDateTo = DateTime.now();
+          var dateFromNew = new DateFormat('yyyy-MM-dd').format(parsedDateFrom);
+          var dateToNew = new DateFormat('yyyy-MM-dd').format(parsedDateTo);
+          return Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 18),
+                child: FormHeading(heading: "Add dates of the event."),
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                  elevation: 4.0,
+                  onPressed: () {
+                    DatePicker.showDatePicker(
+                      context,
+                      theme: DatePickerTheme(containerHeight: 210.0),
+                      showTitleActions: true,
+                      minTime: DateTime(2000, 1, 1),
+                      maxTime: DateTime(2022, 12, 31),
+                      onConfirm: (date) {
+                        data.setEventFromDate(
+                            ('${date.year} - ${date.month} - ${date.day}'));
+                        // setState(() {});
+                      },
+                      currentTime: DateTime.now(),
+                      locale: LocaleType.en,
+                    );
+                  },
+                  child: Observer(
+                    builder: (BuildContext context) => Container(
+                      alignment: Alignment.center,
+                      height: 50.0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Container(
+                                child: Row(
+                                  children: <Widget>[
+                                    Icon(Icons.date_range,
+                                        size: 18.0, color: Color(0xFF072247)),
+                                    Text(
+                                      " From: " +
+                                          (data.dateFrom.value.isEmpty
+                                              ? dateFromNew
+                                              : data.dateFrom.value),
+                                      style: TextStyle(
+                                          color: Color(0xFF072247),
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18.0),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                          Text(
+                            "Change",
+                            style: TextStyle(
+                                color: Color(0xFF072247),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18.0),
+                          ),
+                        ],
+                      ),
                     ),
-                    Icon(Icons.calendar_today, size: 30),
-                    Text(
-                      dateFromNew,
-                      // DateTime.pa(data.dateFrom.value),
-                      style: TextStyle(fontSize: 20, color: Color(0xFF072247)),
-                    ),
-                  ],
+                  ),
+                  color: Colors.white,
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      "To:",
-                      style: TextStyle(fontSize: 20, color: Color(0xFF072247)),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                  elevation: 4.0,
+                  onPressed: () {
+                    DatePicker.showDatePicker(
+                      context,
+                      theme: DatePickerTheme(containerHeight: 210.0),
+                      showTitleActions: true,
+                      minTime: DateTime(2000, 1, 1),
+                      maxTime: DateTime(2022, 12, 31),
+                      onConfirm: (date) {
+                        data.setEventToDate(
+                            '${date.year} - ${date.month} - ${date.day}');
+                      },
+                      currentTime: DateTime.now(),
+                      locale: LocaleType.en,
+                    );
+                  },
+                  child: Observer(
+                    builder: (BuildContext context) => Container(
+                      alignment: Alignment.center,
+                      height: 50.0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Container(
+                                child: Row(
+                                  children: <Widget>[
+                                    Icon(Icons.date_range,
+                                        size: 18.0, color: Color(0xFF072247)),
+                                    Text(
+                                      " To: " +
+                                          (data.dateTo.value.isEmpty
+                                              ? dateToNew
+                                              : data.dateTo.value),
+                                      style: TextStyle(
+                                          color: Color(0xFF072247),
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18.0),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                          Text(
+                            "Change",
+                            style: TextStyle(
+                                color: Color(0xFF072247),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18.0),
+                          ),
+                        ],
+                      ),
                     ),
-                    Icon(Icons.calendar_today, size: 30),
-                    Text(
-                      dateToNew,
-                      style: TextStyle(fontSize: 20, color: Color(0xFF072247)),
-                    ),
-                  ],
+                  ),
+                  color: Colors.white,
                 ),
-              ],
-            )
-          ],
-        );
-      }),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
