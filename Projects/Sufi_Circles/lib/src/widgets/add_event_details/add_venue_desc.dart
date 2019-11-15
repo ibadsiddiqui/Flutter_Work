8cdc972737d005ac10539d8f01c1f4775de2882c
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-11-16 01:16:33 +0500

    --Commit: venue details added

--- Projects/Sufi_Circles/lib/src/widgets/add_event_details/add_venue_desc.dart
+++ Projects/Sufi_Circles/lib/src/widgets/add_event_details/add_venue_desc.dart
@@ -1,7 +1,5 @@
+import 'package:Sufi_Circles/src/widgets/add_event_details/form/form_heading.dart';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-// import 'package:provider/provider.dart';
-// import 'package:Sufi_Circles/src/models/event/EventModel.dart';
 
 class AddVenueDesc extends StatefulWidget {
   @override
@@ -9,138 +7,85 @@ class AddVenueDesc extends StatefulWidget {
 }
 
 class _AddVenueDescState extends State<AddVenueDesc> {
-  GoogleMapController mapController;
-
-  final LatLng _center = const LatLng(45.521563, -122.677433);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
-
-  GoogleMapController myMapController;
-  final Set<Marker> _markers = new Set();
-  static const LatLng _mainLocation = const LatLng(25.69893, 32.6421);
-
-  Set<Marker> myMarker() {
-    setState(() {
-      _markers.add(Marker(
-        // This marker id can be anything that uniquely identifies each marker.
-        markerId: MarkerId(_mainLocation.toString()),
-        position: _mainLocation,
-        infoWindow: InfoWindow(
-          title: 'Historical City',
-          snippet: '5 Star Rating',
-        ),
-        icon: BitmapDescriptor.defaultMarker,
-      ));
-    });
-
-    return _markers;
-  }
-
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     // EventModel eventModel = Provider.of<EventModel>(context);
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Maps With Marker'),
-              backgroundColor: Colors.blue[900],
+    return Container(
+      margin: EdgeInsets.only(top: size.width * 0.3),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 18),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            FormHeading(heading: "Add venue details."),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 18, vertical: 20.0),
+              child: Text(
+                "Please select from options below on how do you want to add the venue details?",
+                style: TextStyle(fontSize: 18.0, fontFamily: "CreteRound"),
+              ),
             ),
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: GoogleMap(
-                    initialCameraPosition: CameraPosition(
-                      target: _mainLocation,
-                      zoom: 10.0,
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0)),
+              elevation: 4.0,
+              onPressed: () {},
+              child: Container(
+                alignment: Alignment.center,
+                height: 50.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.map, size: 20.0, color: Color(0xFF072247)),
+                    Text(
+                      " Add using map ",
+                      style: TextStyle(
+                          color: Color(0xFF072247),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0),
                     ),
-                    markers: this.myMarker(),
-                    mapType: MapType.normal,
-                    onMapCreated: (controller) {
-                      setState(() {
-                        myMapController = controller;
-                      });
-                    },
-                  ),
+                  ],
                 ),
-              ],
-            )));
-    // Container(
-    //   child: Padding(
-    //     padding: const EdgeInsets.symmetric(horizontal: 18),
-    //     child: Column(
-    //       crossAxisAlignment: CrossAxisAlignment.start,
-    //       children: <Widget>[
-    //         FormHeading(heading: "Add venue details."),
-    //         Padding(
-    //           padding: EdgeInsets.symmetric(horizontal: 18, vertical: 20.0),
-    //           child: Text(
-    //             "Please select from options below on how do you want to add the venue details?",
-    //             style: TextStyle(fontSize: 18.0, fontFamily: "CreteRound"),
-    //           ),
-    //         ),
-    //         RaisedButton(
-    //           shape: RoundedRectangleBorder(
-    //               borderRadius: BorderRadius.circular(5.0)),
-    //           elevation: 4.0,
-    //           onPressed: () {},
-    //           child: Container(
-    //             alignment: Alignment.center,
-    //             height: 50.0,
-    //             child: Row(
-    //               mainAxisAlignment: MainAxisAlignment.center,
-    //               children: <Widget>[
-    //                 Icon(Icons.map, size: 20.0, color: Color(0xFF072247)),
-    //                 Text(
-    //                   " Add using map ",
-    //                   style: TextStyle(
-    //                       color: Color(0xFF072247),
-    //                       fontWeight: FontWeight.bold,
-    //                       fontSize: 18.0),
-    //                 ),
-    //               ],
-    //             ),
-    //           ),
-    //           color: Colors.white,
-    //         ),
-    //         Container(
-    //           padding: EdgeInsets.symmetric(vertical: 5.0),
-    //           alignment: Alignment.center,
-    //           child: Text(
-    //             "OR",
-    //             textAlign: TextAlign.center,
-    //             style: TextStyle(fontSize: 18.0, fontFamily: "CreteRound"),
-    //           ),
-    //         ),
-    //         RaisedButton(
-    //           shape: RoundedRectangleBorder(
-    //               borderRadius: BorderRadius.circular(5.0)),
-    //           elevation: 4.0,
-    //           onPressed: () {},
-    //           child: Container(
-    //             alignment: Alignment.center,
-    //             height: 50.0,
-    //             child: Row(
-    //               mainAxisAlignment: MainAxisAlignment.center,
-    //               children: <Widget>[
-    //                 Icon(Icons.map, size: 20.0, color: Color(0xFF072247)),
-    //                 Text(
-    //                   " Add details using inputs",
-    //                   style: TextStyle(
-    //                       color: Color(0xFF072247),
-    //                       fontWeight: FontWeight.bold,
-    //                       fontSize: 18.0),
-    //                 ),
-    //               ],
-    //             ),
-    //           ),
-    //           color: Colors.white,
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    // );
+              ),
+              color: Colors.white,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 5.0),
+              alignment: Alignment.center,
+              child: Text(
+                "OR",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18.0, fontFamily: "CreteRound"),
+              ),
+            ),
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0)),
+              elevation: 4.0,
+              onPressed: () {},
+              child: Container(
+                alignment: Alignment.center,
+                height: 50.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.map, size: 20.0, color: Color(0xFF072247)),
+                    Text(
+                      " Add details using inputs",
+                      style: TextStyle(
+                          color: Color(0xFF072247),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0),
+                    ),
+                  ],
+                ),
+              ),
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
