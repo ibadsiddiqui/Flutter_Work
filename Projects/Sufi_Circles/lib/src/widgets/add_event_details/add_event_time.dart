d7721c5008b370ec8ff9fb8700db80e3a67ef86c
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-11-15 16:32:45 +0500

    --Commit: time picker refactored

--- Projects/Sufi_Circles/lib/src/widgets/add_event_details/add_event_time.dart
+++ Projects/Sufi_Circles/lib/src/widgets/add_event_details/add_event_time.dart
@@ -1,11 +1,11 @@
+import 'package:Sufi_Circles/src/models/event/EventModel.dart';
 import 'package:Sufi_Circles/src/widgets/add_event_details/event_date_widgets/time_picker.dart';
+import 'package:Sufi_Circles/src/widgets/add_event_details/form/form_heading.dart';
 import 'package:Sufi_Circles/src/widgets/buttons/round_clipped_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
-import 'package:Sufi_Circles/src/models/event/EventModel.dart';
-import 'package:Sufi_Circles/src/widgets/add_event_details/form/form_heading.dart';
 
 class AddEventTime extends StatelessWidget {
   const AddEventTime({Key key}) : super(key: key);
@@ -24,37 +24,26 @@ class AddEventTime extends StatelessWidget {
                 padding: const EdgeInsets.symmetric(horizontal: 18),
                 child: FormHeading(heading: "Add time of the event."),
               ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 18, vertical: 10.0),
-                child: Text(
-                  "At what time will it start?",
-                  style: TextStyle(fontSize: 18.0, fontFamily: "CreteRound"),
-                ),
-              ),
               Observer(
                 builder: (_) => TimePicker(
-                    hour: data.startTime.value["hour"],
-                    minute: data.startTime.value["minute"],
-                    onPressed: () => DatePicker.showTimePicker(
-                          context,
-                          theme: DatePickerTheme(containerHeight: 210.0),
-                          showTitleActions: true,
-                          onConfirm: (time) =>
-                              data.setEventStartTime(time.hour, time.minute),
-                          currentTime: DateTime.now().toLocal(),
-                          locale: LocaleType.en,
-                        )),
+                  heading: "At what time will it start?",
+                  hour: data.startTime.value["hour"],
+                  minute: data.startTime.value["minute"],
+                  onPressed: () => DatePicker.showTimePicker(
+                    context,
+                    theme: DatePickerTheme(containerHeight: 210.0),
+                    showTitleActions: true,
+                    onConfirm: (time) =>
+                        data.setEventStartTime(time.hour, time.minute),
+                    currentTime: DateTime.now().toLocal(),
+                    locale: LocaleType.en,
+                  ),
+                ),
               ),
               SizedBox(height: 20),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 18, vertical: 10.0),
-                child: Text(
-                  "At what time will it end?",
-                  style: TextStyle(fontSize: 18.0, fontFamily: "CreteRound"),
-                ),
-              ),
               Observer(
                 builder: (_) => TimePicker(
+                    heading: "At what time will it end?",
                     hour: data.startTime.value["hour"],
                     minute: data.startTime.value["minute"],
                     onPressed: () => DatePicker.showTimePicker(
--- Projects/Sufi_Circles/lib/src/widgets/add_event_details/event_date_widgets/time_picker.dart
+++ Projects/Sufi_Circles/lib/src/widgets/add_event_details/event_date_widgets/time_picker.dart
@@ -1,59 +1,72 @@
+import 'package:Sufi_Circles/src/widgets/add_event_details/event_date_widgets/picker_text.dart';
 import 'package:flutter/material.dart';
 
 class TimePicker extends StatelessWidget {
   final Function onPressed;
   final int hour;
   final int minute;
-  const TimePicker({Key key, this.onPressed, this.hour, this.minute})
+  final String heading;
+
+  const TimePicker(
+      {Key key, this.onPressed, this.hour, this.minute, this.heading})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 20),
-      child: RaisedButton(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
-        elevation: 4.0,
-        onPressed: () => onPressed(),
-        child: Container(
-          alignment: Alignment.center,
-          height: 50.0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Row(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          PickerText(
+            text: heading,
+          ),
+          RaisedButton(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0)),
+            elevation: 4.0,
+            onPressed: () => onPressed(),
+            child: Container(
+              alignment: Alignment.center,
+              height: 50.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Container(
-                    child: Row(
-                      children: <Widget>[
-                        Icon(Icons.date_range,
-                            size: 18.0, color: Color(0xFF072247)),
-                        Text(
-                          " Hour: " +
-                              hour.toString() +
-                              " Min: " +
-                              minute.toString(),
-                          style: TextStyle(
-                              color: Color(0xFF072247),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18.0),
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.date_range,
+                                size: 18.0, color: Color(0xFF072247)),
+                            Text(
+                              " Hour: " +
+                                  hour.toString() +
+                                  " Min: " +
+                                  minute.toString(),
+                              style: TextStyle(
+                                  color: Color(0xFF072247),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18.0),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  )
+                      )
+                    ],
+                  ),
+                  Text(
+                    "Change",
+                    style: TextStyle(
+                        color: Color(0xFF072247),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0),
+                  ),
                 ],
               ),
-              Text(
-                "Change",
-                style: TextStyle(
-                    color: Color(0xFF072247),
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.0),
-              ),
-            ],
+            ),
+            color: Colors.white,
           ),
-        ),
-        color: Colors.white,
+        ],
       ),
     );
   }
