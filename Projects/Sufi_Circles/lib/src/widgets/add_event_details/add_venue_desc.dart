f4f85715dd635882e13daa065b317d7c67669553
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-11-15 17:46:22 +0500

    --commit: permission added

--- Projects/Sufi_Circles/lib/src/widgets/add_event_details/add_venue_desc.dart
+++ Projects/Sufi_Circles/lib/src/widgets/add_event_details/add_venue_desc.dart
@@ -1,6 +1,3 @@
-import 'dart:async';
-
-import 'package:Sufi_Circles/src/widgets/add_event_details/form/form_heading.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 // import 'package:provider/provider.dart';
@@ -20,24 +17,56 @@ class _AddVenueDescState extends State<AddVenueDesc> {
     mapController = controller;
   }
 
+  GoogleMapController myMapController;
+  final Set<Marker> _markers = new Set();
+  static const LatLng _mainLocation = const LatLng(25.69893, 32.6421);
+
+  Set<Marker> myMarker() {
+    setState(() {
+      _markers.add(Marker(
+        // This marker id can be anything that uniquely identifies each marker.
+        markerId: MarkerId(_mainLocation.toString()),
+        position: _mainLocation,
+        infoWindow: InfoWindow(
+          title: 'Historical City',
+          snippet: '5 Star Rating',
+        ),
+        icon: BitmapDescriptor.defaultMarker,
+      ));
+    });
+
+    return _markers;
+  }
+
   @override
   Widget build(BuildContext context) {
     // EventModel eventModel = Provider.of<EventModel>(context);
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Maps With Marker'),
+              backgroundColor: Colors.blue[900],
+            ),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: GoogleMap(
+                    initialCameraPosition: CameraPosition(
+                      target: _mainLocation,
+                      zoom: 10.0,
+                    ),
+                    markers: this.myMarker(),
+                    mapType: MapType.normal,
+                    onMapCreated: (controller) {
+                      setState(() {
+                        myMapController = controller;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            )));
     // Container(
     //   child: Padding(
     //     padding: const EdgeInsets.symmetric(horizontal: 18),
