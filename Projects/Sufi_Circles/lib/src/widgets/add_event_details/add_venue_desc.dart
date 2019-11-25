eee9797644a7cff4954e7e2aa7aa4d121ec28b01
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-11-26 00:55:28 +0500

    --Commit: lat long rendered

--- Projects/Sufi_Circles/lib/src/widgets/add_event_details/add_venue_desc.dart
+++ Projects/Sufi_Circles/lib/src/widgets/add_event_details/add_venue_desc.dart
@@ -1,8 +1,11 @@
 import 'package:Sufi_Circles/src/models/event/EventModel.dart';
 import 'package:Sufi_Circles/src/navigator/auth_navigator.dart';
 import 'package:Sufi_Circles/src/pages/map_view/MapView.dart';
+import 'package:Sufi_Circles/src/widgets/add_event_details/event_date_widgets/picker_text.dart';
+import 'package:Sufi_Circles/src/widgets/add_event_details/form/form_heading.dart';
 import 'package:Sufi_Circles/src/widgets/add_event_details/venue_desc_widgets/show_type_selection_for_venue.dart';
 import 'package:Sufi_Circles/src/widgets/add_event_details/venue_desc_widgets/venue_detail_form.dart';
+import 'package:Sufi_Circles/src/widgets/buttons/round_clipped_button.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:provider/provider.dart';
@@ -26,9 +29,87 @@ class _AddVenueDescState extends State<AddVenueDesc> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final venueDetails = Provider.of<EventModel>(context);
+
     switch (selectionType) {
       case "View Details From Maps":
-        return Container();
+        return Container(
+          margin: EdgeInsets.only(top: size.width * 0.1),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                FormHeading(heading: "Add venue details."),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 18),
+                  child:
+                      PickerText(text: "following is the data from the map..."),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    // crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Text(
+                        "Lat/Long",
+                        style:
+                            TextStyle(fontSize: 17.0, fontFamily: "CreteRound"),
+                      ),
+                      Text(
+                        venueDetails.locationDetails.value["lat"].toString() +
+                            " / " +
+                            venueDetails.locationDetails.value["long"]
+                                .toString(),
+                        style:
+                            TextStyle(fontSize: 17.0, fontFamily: "CreteRound"),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    // crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Text(
+                        "Lat/Long",
+                        style:
+                            TextStyle(fontSize: 17.0, fontFamily: "CreteRound"),
+                      ),
+                      Text(
+                        venueDetails.locationDetails.value["lat"].toString() +
+                            " / " +
+                            venueDetails.locationDetails.value["long"]
+                                .toString(),
+                        style:
+                            TextStyle(fontSize: 17.0, fontFamily: "CreteRound"),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    RoundClippedButton(
+                      isMain: true,
+                      onPress: () => {},
+                      title: "cancel",
+                      child: Icon(Icons.close, color: Colors.white),
+                    ),
+                    SizedBox(width: size.width * .2),
+                    RoundClippedButton(isMain: false, onPress: () {}),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
       case "Inputs":
         return VenueDetailForm(toggleSelectionType: setSelectionType);
       default:
