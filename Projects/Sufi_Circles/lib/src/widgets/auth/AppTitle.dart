dbad2a0543ccb482cbc1d91046b31cc1c0c8aa9e
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-11-29 20:49:12 +0500

    --Commit: Color typo fixed for Title

--- Projects/Sufi_Circles/lib/src/widgets/auth/AppTitle.dart
+++ Projects/Sufi_Circles/lib/src/widgets/auth/AppTitle.dart
@@ -3,23 +3,18 @@ import 'package:flutter/material.dart';
 class AppTitle extends StatelessWidget {
   final Color color;
 
-  const AppTitle({Key key, this.color}) : super(key: key);
+  AppTitle({Key key, this.color}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    print(color);
     return new Padding(
       padding: const EdgeInsets.only(bottom: 16.0, top: 5.0),
-      child: color != null
-          ? new Text(
-              "Sufi Circles",
-              style: Theme.of(context).textTheme.headline,
-            )
-          : new Text(
-              "Sufi Circles",
-              style: TextStyle(
-                  color: Color.fromRGBO(8, 51, 88, 1),
-                  fontSize: 40,
-                  fontFamily: "CreteRound"),
-            ),
+      child: new Text(
+        "Sufi Circles",
+        style: color == null
+            ? Theme.of(context).textTheme.headline
+            : TextStyle(color: color, fontSize: 40, fontFamily: "CreteRound"),
+      ),
     );
   }
 }
