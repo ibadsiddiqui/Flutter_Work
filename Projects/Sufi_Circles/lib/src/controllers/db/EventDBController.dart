5794537bb7cf353a885b1d204a13c70d46f6a293
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2020-01-19 02:05:11 +0500

    --Commit: Controller added for event

---
+++ Projects/Sufi_Circles/lib/src/controllers/db/EventDBController.dart
@@ -0,0 +1,37 @@
+import 'package:Sufi_Circles/src/models/event/EventModel.dart';
+import 'package:Sufi_Circles/src/services/db/EventDBServices.dart';
+import 'package:Sufi_Circles/src/services/storage/ImageStorage.dart';
+import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
+
+class EventDBController {
+  EventDBService _eventDBService = new EventDBService();
+  ImageStorage _imageStorage = new ImageStorage();
+  Uuid uuid = new Uuid();
+
+  Future<bool> createEvent(context) async {
+    try {
+      String eventID = uuid.v4();
+      List<String> venuePhotosList = [];
+      EventModel eventModel = Provider.of<EventModel>(context);
+      Map<String, dynamic> eventData = eventModel.getEventModel();
+      String coverPhotoURL = await _imageStorage.uploadEventPhoto(
+          "cover", eventID, eventModel.eventCoverPhoto);
+      if (!eventModel.isVenuePhotosEmpty)
+        for (String url in eventModel.eventVenuePhoto) {
+          String path =
+              await _imageStorage.uploadEventPhoto("photos", eventID, url);
+          venuePhotosList.add(path);
+        }
+      eventData.addAll({
+        "eventID": eventID,
+        "coverPhotoURL": coverPhotoURL,
+        "venuePhotosURLList": venuePhotosList,
+      });
+      await _eventDBService.createEvent(eventModel.getEventModel());
+      return true;
+    } catch (e) {
+      throw e;
+    }
+  }
+}
