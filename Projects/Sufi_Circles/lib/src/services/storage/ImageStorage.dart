0cc5e49ded7092d3d1868bcac3346b10b7e791bd
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2020-01-19 02:04:47 +0500

    --Commit: upload event photo functiona added for Image storage

--- Projects/Sufi_Circles/lib/src/services/storage/ImageStorage.dart
+++ Projects/Sufi_Circles/lib/src/services/storage/ImageStorage.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
 import 'package:Sufi_Circles/src/services/db/UserDBServices.dart';
+import 'package:Sufi_Circles/src/utils/string_helper.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:path/path.dart';
 
 class ImageStorage {
   StorageReference storageRef = FirebaseStorage.instance.ref();
@@ -17,6 +19,22 @@ class ImageStorage {
     StorageTaskSnapshot downloadUrl = (await uploadTask.onComplete);
     String url = (await downloadUrl.ref.getDownloadURL());
     userDBServices.updateUserProfilePicture(userID, url);
+    return url;
+  }
+
+  Future<String> uploadEventPhoto(folder, eventID, filePath) async {
+    StorageUploadTask uploadTask = storageRef
+        .child("events")
+        .child(eventID)
+        .child(folder)
+        .child(getFilename(filePath))
+        .putFile(
+          File(filePath),
+          StorageMetadata(
+              contentType: "image" + '/' + "jpg", contentLanguage: "en"),
+        );
+    StorageTaskSnapshot downloadUrl = (await uploadTask.onComplete);
+    String url = (await downloadUrl.ref.getDownloadURL());
     return url;
   }
 }
