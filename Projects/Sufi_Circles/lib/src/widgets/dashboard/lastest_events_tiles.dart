8522b99ed0e5421c457275bec83f82f8d6a87d45
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2020-01-20 11:53:36 +0500

    --Commit: snapshot mapped

--- Projects/Sufi_Circles/lib/src/widgets/dashboard/lastest_events_tiles.dart
+++ Projects/Sufi_Circles/lib/src/widgets/dashboard/lastest_events_tiles.dart
@@ -1,22 +1,22 @@
-import 'package:Sufi_Circles/src/models/events_list/EventsListModel.dart';
-import 'package:Sufi_Circles/src/models/recommended_events/RecommendedEventsModel.dart';
 import 'package:Sufi_Circles/src/utils/model_helper_methods.dart';
+import 'package:Sufi_Circles/src/utils/string_helper.dart';
 import 'package:Sufi_Circles/src/widgets/dashboard/background.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class LatestEventTiles extends StatelessWidget {
   final int index;
+  final List<DocumentSnapshot> snapshots;
 
-  const LatestEventTiles({Key key, this.index}) : super(key: key);
+  LatestEventTiles({Key key, this.index, this.snapshots}) : super(key: key);
 
   Widget _buildEventHeading(context, String title) {
     return Container(
       alignment: Alignment.center,
       width: title.length.toDouble() * 20,
       child: Text(
-        title,
-        style: Theme.of(context).textTheme.display1.apply(color: Colors.white),
+        truncateStrings(title, 15),
+        style: Theme.of(context).textTheme.display1,
       ),
     );
   }
@@ -35,17 +35,16 @@ class LatestEventTiles extends StatelessWidget {
   Widget _buildEventDesc(context, String desc) {
     return Text(
       desc,
-      style: Theme.of(context).textTheme.body1,
+      style: Theme.of(context).textTheme.body1.apply(color: Colors.black),
       textAlign: TextAlign.center,
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    EventsListModel eventModel = Provider.of<EventsListModel>(context);
     final size = MediaQuery.of(context).size;
-    if (eventModel.allEvents.length - 1 > index) {
-      final detail = getEventDetailsUsingIndex(eventModel.allEvents[index]);
+    if (this.snapshots.length - 1 > index) {
+      final detail = getEventDetailsUsingIndex(this.snapshots[index]);
       return new Container(
         padding: const EdgeInsets.symmetric(vertical: 10.0),
         child: ClipRRect(
