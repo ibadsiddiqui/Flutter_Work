a695c13b9cf63e381d429b100d2fc571848d5fac
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2020-03-09 23:46:16 +0500

    --Commit: search screen completed

--- Projects/Sufi_Circles/lib/src/pages/search_events/search_events.dart
+++ Projects/Sufi_Circles/lib/src/pages/search_events/search_events.dart
@@ -1,4 +1,5 @@
 import 'package:Sufi_Circles/src/controllers/db/EventDBController.dart';
+import 'package:Sufi_Circles/src/utils/string_helper.dart';
 import 'package:Sufi_Circles/src/widgets/dashboard/heading.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -16,6 +17,7 @@ class _SearchEventsState extends State<SearchEvents> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
@@ -49,24 +51,55 @@ class _SearchEventsState extends State<SearchEvents> {
                 ),
               ),
               Container(
-                padding: EdgeInsets.symmetric(vertical: 10),
-                height: 100,
+                margin: EdgeInsets.only(top: size.height * 0.05),
+                height: size.height * 0.75,
                 child: StreamBuilder<QuerySnapshot>(
-                    stream: _eventDBController.getAllEvent(),
-                    builder: (context, snapshot) {
-                      return !snapshot.hasData
-                          ? Container()
-                          : ListView.builder(
-                              itemCount: snapshot.data.documents.length,
-                              itemBuilder: (context, idx) {
-                                String name =
-                                    (snapshot.data.documents[idx])['name'];
-                                    // TODO:  fix tihs
-                                return Text(name,
-                                    style: TextStyle(color: Colors.black));
-                              },
+                  stream: _eventDBController.getAllEvent(),
+                  builder: (context, snapshot) => !snapshot.hasData
+                      ? Container()
+                      : ListView.builder(
+                          itemCount: snapshot.data.documents.length,
+                          itemBuilder: (_, idx) {
+                            Map<String, dynamic> document =
+                                reversedArray(snapshot.data.documents)[idx]
+                                    .data;
+                            return Container(
+                              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                              height: 120,
+                              width: double.maxFinite,
+                              child: Card(
+                                elevation: 5,
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: <Widget>[
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(40.0)),
+                                      child: Image.network(
+                                        document["coverPhotoURL"],
+                                        width: size.width * 0.2,
+                                        height: size.height * 0.1,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                    Container(
+                                        width: 100,
+                                        child: Text(
+                                          document["name"],
+                                          style:
+                                              Theme.of(context).textTheme.body2,
+                                          textAlign: TextAlign.left,
+                                          textDirection: TextDirection.ltr,
+                                        ))
+                                  ],
+                                ),
+                              ),
                             );
-                    }),
+                          },
+                        ),
+                ),
               ),
             ],
           ),
