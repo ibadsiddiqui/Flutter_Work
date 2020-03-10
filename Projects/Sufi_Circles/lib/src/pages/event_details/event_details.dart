dcf646ac7c5981d79c09574dd67a828afab82538
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2020-03-10 23:51:52 +0500

    --Commit: style refactored

--- Projects/Sufi_Circles/lib/src/pages/event_details/event_details.dart
+++ Projects/Sufi_Circles/lib/src/pages/event_details/event_details.dart
@@ -1,5 +1,7 @@
-import 'package:Sufi_Circles/src/widgets/profile/user_picture_background.dart';
+import 'package:Sufi_Circles/src/utils/date_helper.dart';
+import 'package:Sufi_Circles/src/widgets/dashboard/background.dart';
 import 'package:flutter/material.dart';
+import 'package:timeago/timeago.dart';
 
 class EventDetails extends StatelessWidget {
   final Map<String, dynamic> event;
@@ -9,32 +11,36 @@ class EventDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+    String startDate = format(formatTimestamp(event["dateFrom"]));
+    Map<String, int> startTime = event["starTime"];
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            SliverAppBar(
-              expandedHeight: size.height * 0.45,
-              pinned: true,
-              backgroundColor: Color(0xFF072247),
-              flexibleSpace: FlexibleSpaceBar(
-                centerTitle: true,
-                background: HeroAnimation(photoPath: event["coverPhotoURL"]),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.white.withOpacity(0.01),
+        elevation: 0,
+      ),
+      body: Container(
+        child: Container(
+          decoration: dashboardTopTabBackground(path: event["coverPhotoURL"]),
+          height: size.height * 0.4,
+          width: size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Text(
+                event["name"],
+                style: Theme.of(context)
+                    .textTheme
+                    .display1
+                    .apply(color: Colors.white),
               ),
-            ),
-          ];
-        },
-        body: Container(
-          padding: EdgeInsets.only(top: size.height * 0.125),
-          child: new GestureDetector(
-            onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
-            child: Column(
-              children: <Widget>[
-                Text(event["name"], style: Theme.of(context).textTheme.body2),
-                Text(event["desc"], style: Theme.of(context).textTheme.body2),
-              ],
-            ),
+              Container(
+                color: Colors.red,
+                padding: EdgeInsets.all(10),
+                child: Text('$startDate $startTime["hour"] - $startTime["minute"]'),
+              )
+            ],
           ),
         ),
       ),
