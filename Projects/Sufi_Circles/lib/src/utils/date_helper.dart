651771d5ef1d5aac76e60eb5b5a3360e0f451a29
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2020-03-10 23:52:03 +0500

    --Commit: date helper added

--- Projects/Sufi_Circles/lib/src/utils/date_helper.dart
+++ Projects/Sufi_Circles/lib/src/utils/date_helper.dart
@@ -1,4 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
+
 DateTime getCurrentDate() {
   DateTime now = new DateTime.now();
   return new DateTime(now.year, now.month, now.day);
+}
+
+DateTime formatTimestamp(Timestamp timestamp) {
+  return DateTime.parse(timestamp.toDate().toString());
 }
