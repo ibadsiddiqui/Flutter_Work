b58e0b0dee579bc719842854ed8304375bf86781
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2020-03-19 13:11:54 +0500

    --Commit: Collection name updated

--- Projects/Sufi_Circles/lib/src/services/db/UserDBServices.dart
+++ Projects/Sufi_Circles/lib/src/services/db/UserDBServices.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserDBServices {
-  CollectionReference userDB = Firestore.instance.collection("user");
+  CollectionReference userDB = Firestore.instance.collection("users");
   Future<void> createUserInDB(Map<String, dynamic> user) async {
     try {
       return (await userDB.document(user["uid"]).setData(user));
